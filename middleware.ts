525dbf4a6adf43a76e683a6814c1faa0ec3e3c92
shubhamshinare <shubhamshinare2004@gmail.com>
2025-04-03 00:12:09 +0530

    Fixed dependency conflicts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,26 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const protectedRoute = createRouteMatcher([
-  '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
+  "/",
+  "/upcoming",
+  "/meeting(.*)",
+  "/previous",
+  "/recordings",
+  "/personal-room",
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRoute(req)) {
+    const { userId } = await auth();
+    if (!userId) {
+      const signInUrl = new URL("/sign-in", req.url);
+      signInUrl.searchParams.set("returnBackUrl", req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+  }
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
